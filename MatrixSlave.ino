6641897fabbe3ecc3e19749d38fca792b8410a9d
Marian Marinov <mm@yuhu.biz>
2020-09-01 13:30:36 +0300

    MatrixSlave.ino: Removed "while (state ..." and all useless initializations of t=0
    Signed-off-by: Marian Marinov <mm@yuhu.biz>

--- MatrixSlave.ino
+++ MatrixSlave.ino
@@ -41,179 +41,169 @@ void light_leds(int start, int end) {
 }
 
 void LeftArrowBlink() { // Function that is called when Vx is <= 100 and shows a Left Blinking Arrow on the matrix
-	while (state == 'L') {
+	FastLED.clear();
+	light_leds(6,24);
+	light_leds(40,54);
+	light_leds(72,88);
+	light_leds(102,122);
+	light_leds(132,144);
+	light_leds(150,156);
+	light_leds(162,168);
+	light_leds(184,190);
+	light_leds(192,198);
+	light_leds(218,222);
+	light_leds(226,228);
+	light_leds(252,252);
+	FastLED.show();
+	delay(1000);
+	FastLED.clear();
+	light_leds(0,63);
+	light_leds(76,83);
+	light_leds(108,115);
+	light_leds(119,136);
+	light_leds(140,147);
+	light_leds(151,168);
+	light_leds(172,179);
+	light_leds(183,186);
+	light_leds(197,200);
+	light_leds(204,211);
+	light_leds(215,218);
+	light_leds(229,232);
+	light_leds(236,243);
+	light_leds(247,250);
+	FastLED.show();
+	delay(1000);
+	FastLED.clear();
+	for (t=0; t<3; t++) {
 		FastLED.clear();
-		light_leds(6,24);
-		light_leds(40,54);
-		light_leds(72,88);
-		light_leds(102,122);
-		light_leds(132,144);
-		light_leds(150,156);
-		light_leds(162,168);
-		light_leds(184,190);
-		light_leds(192,198);
-		light_leds(218,222);
-		light_leds(226,228);
-		light_leds(252,252);
+		light_leds(119,136);
+		light_leds(151,168);
+		light_leds(183,186);
+		light_leds(197,200);
+		light_leds(215,218);
+		light_leds(229,232);
+		light_leds(247,250);
 		FastLED.show();
-		delay(1000);
+		delay(300);
 		FastLED.clear();
 		light_leds(0,63);
 		light_leds(76,83);
 		light_leds(108,115);
-		light_leds(119,136);
 		light_leds(140,147);
-		light_leds(151,168);
 		light_leds(172,179);
-		light_leds(183,186);
-		light_leds(197,200);
 		light_leds(204,211);
-		light_leds(215,218);
-		light_leds(229,232);
 		light_leds(236,243);
-		light_leds(247,250);
 		FastLED.show();
-		delay(1000);
+		delay(300);
 		FastLED.clear();
-		for (t=0; t<3; t++) {
-			FastLED.clear();
-			light_leds(119,136);
-			light_leds(151,168);
-			light_leds(183,186);
-			light_leds(197,200);
-			light_leds(215,218);
-			light_leds(229,232);
-			light_leds(247,250);
-			FastLED.show();
-			delay(300);
-			FastLED.clear();
-			light_leds(0,63);
-			light_leds(76,83);
-			light_leds(108,115);
-			light_leds(140,147);
-			light_leds(172,179);
-			light_leds(204,211);
-			light_leds(236,243);
-			FastLED.show();
-			delay(300);
-			FastLED.clear();
-		}
-		t=0;
-		Serial.write('A');
-	} 
+	}
+	Serial.write('A');
 }
 
 
 void RightArrowBlink()  {// Function that is called when Vx is >= 1000 and shows a Right Blinking Arrow on the matrix
-	while (state == 'R') {
-		FastLED.clear();
-		light_leds(12,12);
-		light_leds(18,20);
-		light_leds(42,54);
-		light_leds(72,78);
-		light_leds(82,88);
-		light_leds(96,96);
-		light_leds(102,108);
-		light_leds(116,122);
-		light_leds(126,130);
-		light_leds(132,138);
-		light_leds(150,168);
-		light_leds(184,198);
-		light_leds(216,232);
-		light_leds(246,255);
-		FastLED.show();
-		delay(1000);
+	FastLED.clear();
+	light_leds(12,12);
+	light_leds(18,20);
+	light_leds(42,54);
+	light_leds(72,78);
+	light_leds(82,88);
+	light_leds(96,96);
+	light_leds(102,108);
+	light_leds(116,122);
+	light_leds(126,130);
+	light_leds(132,138);
+	light_leds(150,168);
+	light_leds(184,198);
+	light_leds(216,232);
+	light_leds(246,255);
+	FastLED.show();
+	delay(1000);
+	FastLED.clear();
+	light_leds(0,3);
+	light_leds(7,10);
+	light_leds(21,24);
+	light_leds(28,35);
+	light_leds(39,42);
+	light_leds(53,56);
+	light_leds(60,67);
+	light_leds(71,74);
+	light_leds(80,88);
+	light_leds(92,99);
+	light_leds(103,143);
+	light_leds(156,163);
+	light_leds(188,255);
+	FastLED.show();
+	delay(1000);
+	FastLED.clear();
+	for (t=0; t<3; t++) {
 		FastLED.clear();
 		light_leds(0,3);
-		light_leds(7,10);
-		light_leds(21,24);
 		light_leds(28,35);
-		light_leds(39,42);
-		light_leds(53,56);
 		light_leds(60,67);
-		light_leds(71,74);
-		light_leds(80,88);
 		light_leds(92,99);
-		light_leds(103,143);
+		light_leds(124,131);
 		light_leds(156,163);
 		light_leds(188,255);
 		FastLED.show();
-		delay(1000);
+		delay(300);
 		FastLED.clear();
-		for (t=0; t<3; t++) {
-			FastLED.clear();
-			light_leds(0,3);
-			light_leds(28,35);
-			light_leds(60,67);
-			light_leds(92,99);
-			light_leds(124,131);
-			light_leds(156,163);
-			light_leds(188,255);
-			FastLED.show();
-			delay(300);
-			FastLED.clear();
-			light_leds(0,3);
-			light_leds(28,35);
-			light_leds(60,67);
-			light_leds(92,99);
-			light_leds(124,131);
-			light_leds(156,163);
-			light_leds(188,255);
-			FastLED.show();
-			delay(300);
-			FastLED.clear();
-		}
-		t=0;
-		Serial.write('D');
+		light_leds(0,3);
+		light_leds(28,35);
+		light_leds(60,67);
+		light_leds(92,99);
+		light_leds(124,131);
+		light_leds(156,163);
+		light_leds(188,255);
+		FastLED.show();
+		delay(300);
+		FastLED.clear();
 	}
+	Serial.write('D');
 }
 
 
 void ExclamationMarkBlink()  {   
-	while (state == 'U') {
-		for (t=0; t<3; t++) {
-			FastLED.clear();
-			light_leds(1,4);
-			light_leds(26,37);
-			light_leds(57,71);
-			light_leds(87,104);
-			light_leds(118,123);
-			light_leds(133,152);
-			light_leds(168,171);
-			light_leds(181,181);
-			light_leds(205,211);
-			light_leds(236,242);
-			FastLED.show();
-			delay(300);
-			FastLED.clear();
-		}
-		t=0;
-		for (t=0; t<3; t++) {
-			FastLED.clear();
-			lights_leds(5,8);
-			lights_leds(23,26);
-			lights_leds(37,40);
-			lights_leds(55,58);
-			lights_leds(69,72);
-			lights_leds(87,104);
-			lights_leds(119,136);
-			FastLED.show();
-			delay(300);
-			FastLED.clear();
-			lights_leds(12,19);
-			lights_leds(44,51);
-			lights_leds(77,83);
-			lights_leds(108,115);
-			lights_leds(140,147);
-			lights_leds(172,179);
-			lights_leds(192,255);
-			FastLED.show();
-			delay(300);
-			FastLED.clear();
-		}
-		t=0;
-		Serial.write('W');
-	}   
+	for (t=0; t<3; t++) {
+		FastLED.clear();
+		light_leds(1,4);
+		light_leds(26,37);
+		light_leds(57,71);
+		light_leds(87,104);
+		light_leds(118,123);
+		light_leds(133,152);
+		light_leds(168,171);
+		light_leds(181,181);
+		light_leds(205,211);
+		light_leds(236,242);
+		FastLED.show();
+		delay(300);
+		FastLED.clear();
+	}
+	for (t=0; t<3; t++) {
+		FastLED.clear();
+		lights_leds(5,8);
+		lights_leds(23,26);
+		lights_leds(37,40);
+		lights_leds(55,58);
+		lights_leds(69,72);
+		lights_leds(87,104);
+		lights_leds(119,136);
+		FastLED.show();
+		delay(300);
+		FastLED.clear();
+		lights_leds(12,19);
+		lights_leds(44,51);
+		lights_leds(77,83);
+		lights_leds(108,115);
+		lights_leds(140,147);
+		lights_leds(172,179);
+		lights_leds(192,255);
+		FastLED.show();
+		delay(300);
+		FastLED.clear();
+	}
+	Serial.write('W');
 }
 
 
